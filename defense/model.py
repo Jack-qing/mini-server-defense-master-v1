1d642e156461f79977053b2e4fe27fc63c74bc99
Jack-qing <37032448+Jack-qing@users.noreply.github.com>
2018-10-31 09:28:28 +0800

    model没有定义内容

--- defense/model.py
+++ defense/model.py
@@ -5,22 +5,3 @@
 # 说明:
 # 总结:
 
-# 工程配置信息
-class Config(object):
-
-    pass
-
-
-# 开发者模式
-class DeveLopmentConfig(Config):
-    DEBUG = True
-
-
-# 生产模式快
-class ProductionConfig(Config):
-    pass
-
-
-config_dict = {
-    "develop": DeveLopmentConfig,
-}
